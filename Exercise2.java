e1beea22bb9ad4fd30ed3a889379723ded17b052
Hazel Mc Carthy <Hazel.McCarthy@students.ittralee.ie>
2020-01-23 09:44:33 +0000

    uuuu

---
+++ Exercise2.java
@@ -0,0 +1,14 @@
+//Exercise2.java
+/*The screen will display the information about one of the Harry Potter books 
+ *including values, titles etc*/
+ 
+ public class Exercise2 {
+ 	public static void main (String args[])
+ 	{
+ 	
+ 	System.out.println("Title: Harry Potter and the Prisoner of Azkaban" +
+ 		"\nAuthor: J.K. Rowling" + "\nPages: 317" + "\nYear of Publication: 1999" +
+ 			"\nPrice: 35.50");
+ 	}
+ 	
+ }
