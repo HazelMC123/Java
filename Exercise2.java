ef9dbe1538ef0b8a90a3479e670cc96aa97a1d93
Hazel Mc Carthy <Hazel.McCarthy@students.ittralee.ie>
2020-01-23 09:58:36 +0000

    Proper

--- Exercise2.java
+++ Exercise2.java
@@ -1,6 +1,7 @@
 //Exercise2.java
 /*The screen will display the information about one of the Harry Potter books 
- *including values, titles etc*/
+ *including values, titles etc
+ I'm just adding more to this multiline comment*/
  
  public class Exercise2 {
  	public static void main (String args[])
